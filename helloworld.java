3783cec23eecc3f54980f687ed69cc2fed5140bc
AMAN <114601866+aman5222@users.noreply.github.com>
2023-01-01 21:48:43 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");// When you enter ln in last of print it adds a new line
+        System.out.print("Hello World"); 
+        System.out.println("Hello World");
+    }
+}
